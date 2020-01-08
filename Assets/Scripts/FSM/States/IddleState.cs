28c41ec50e2c83e0adc536642d946767337175df
Laura <lauravelazquez.lnv@gmail.com>
2020-01-08 11:16:52 -0300

    State Machine Structure

---
+++ Assets/Scripts/FSM/States/IddleState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IddleState : State
+{
+    public override void InitState() {
+        
+    }
+
+    public override void UpdateState(float delta) {
+        
+    }
+
+    public override void ExitState() {
+        
+    }
+}
---
+++ Assets/Scripts/GameEvents.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Events{
+    public class GameEvent  { }
+
+
+}
