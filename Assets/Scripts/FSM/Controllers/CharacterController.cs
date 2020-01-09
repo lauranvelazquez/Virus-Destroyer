d108bbed2219903dbbd55fcbe6dbae1ba0cb3204
Laura <lauravelazquez.lnv@gmail.com>
2020-01-09 00:04:17 -0300

    controllers

---
+++ Assets/Scripts/FSM/Controllers/CharacterController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterController : FSM
+{
+    private BiowareController _biowareController;
+    private HackerController _hackerController;
+    private MagoController _magoController;
+
+
+    [SerializeField] private Button _bioButton;
+    [SerializeField] private Button _hackerButton;
+
+    [SerializeField] private Button _magoButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
